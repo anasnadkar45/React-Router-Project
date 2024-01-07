a121b24b6519802e2ddc3371fb1a6cd85122b602
anasnadkar45 <anasnadkar23@gmial.com>
2024-01-07 11:22:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './components/Home/Home'
+
+function App() {
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
